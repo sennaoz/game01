9544f6e9c8121022a132ecdc285950d2d266cc9e
sennaoz <161388670+sennaoz@users.noreply.github.com>
2024-09-26 23:33:51 +0300

    game02 add

---
+++ game02.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform player;  // Oyuncu karakteri
+    public float moveSpeed = 3f;  // Düşmanın hareket hızı
+    public float chaseRange = 5f;  // Takip etme mesafesi
+    public float attackRange = 1f;  // Saldırı mesafesi
+
+    private bool isChasing = false;
+
+    void Update()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        // Eğer oyuncu belirli bir mesafeye girerse, takip başlar
+        if (distanceToPlayer < chaseRange)
+        {
+            isChasing = true;
+        }
+        else
+        {
+            isChasing = false;
+        }
+
+        // Takip modundaysa, düşman oyuncuya doğru hareket eder
+        if (isChasing)
+        {
+            ChasePlayer();
+        }
+
+       
